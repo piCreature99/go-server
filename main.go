1162d514e0de4ec6d72ac51a0f79746c90c7a3f2
BigDonut <17dtda8@gmail.com>
2025-12-11 11:35:17 +0700

    nnnnhhhh

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"google.golang.org/api/drive/v3"
@@ -12,11 +13,24 @@ import (
 
 func InitDriveService() {
 	ctx := context.Background()
-	// Ensure you have 'credentials.json' in your root folder
-	srv, err := drive.NewService(ctx, option.WithCredentialsFile("credentials.json"))
+
+	// 1. Read the JSON string from the environment variable (Set on Render)
+	credentialsJSON := os.Getenv("GOOGLE_CREDENTIALS")
+
+	if credentialsJSON == "" {
+		// If the variable is missing, log a sever error and halt
+		log.Fatalf("Fatal: GOOGLE_CREDENTIALS environment variable not set. Cannot initialize Google Drive client.")
+	}
+
+	// 2. Use the credentials string (as a byte slice) to create the client
+	// This uses option.WithCredentialsJSON instead of option.WithCredentialsFile
+
+	srv, err := drive.NewService(ctx, option.WithCredentialsJSON([]byte(credentialsJSON)))
+
 	if err != nil {
 		log.Fatalf("Unable to retrieve Drive client: %v", err)
 	}
+
 	DriveService = srv
 	log.Println("Google Drive Service Initalized")
 }
